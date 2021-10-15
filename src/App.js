34d3fae2a07bac0ba85b593233abc968d85dc251
Anis El Hadj Ali <elhadjalianis@gmail.com>
2021-10-15 22:55:41 +0100

    React-State_V0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import avatar from './img/avatar01.png'
+import { Component } from 'react';
+import { IoIosArrowDropright } from 'react-icons/io';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+
+    fullName: "Anis",
+    bio: "bio",
+    imgSrc: <img src={avatar} alt="avatar"/>,
+    profession: "web dev",
+    show: false
+  };
+
+  profileShow = () => {
+    this.setState({ show: !this.state.show })
+  };
+
+  render() {
+    return (
+      <div>
+        <h4 style={{ color: 'blue' }}>SHOW PROFIL : </h4>
+        <button onClick={this.profileShow}>
+          <IoIosArrowDropright style={{ fontSize: "30px", color: "blue" }}></IoIosArrowDropright>
+        </button>
+        {this.state.show ?
+                (<form>
+                  My Name is : {this.state.fullName}<br/>
+                  My bio : {this.state.bio}<br/>
+                  Photo : {this.state.imgSrc}<br/>
+                  Mys profession is: {this.state.profession}
+              </form>)
+        :<forms/>}
+
+        {/* {this.state.show ? (<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>) : (<p>eee</p>)}  */}
+
+      </div>
+    );
+  };
+};
 
 export default App;
